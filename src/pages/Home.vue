d08b7f07a56db88c7055641c52654b20e6a979d3
ajs <140010746+aboodJS@users.noreply.github.com>
2026-03-23 18:41:27 +0300

    added the pokeapi wrapper made a page for showing pokemon info

---
+++ src/pages/Home.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Pokedex } from 'pokeapi-js-wrapper';
+import { onMounted, ref } from 'vue';
+
+const dex = new Pokedex({cacheImages: true})
+
+const pokemon = ref([])
+
+
+
+
+async function getSpecies() {
+  await dex.getPokemonSpeciesList().then(d => pokemon.value = d.results)
+}
+
+onMounted(() => {
+  getSpecies()
+})
+</script>
+
+
+<template>
+  <h1>ajs-dex</h1>
+  <ul>
+    <li v-for="mon in pokemon"><RouterLink :to="{path: `/pokemon/${mon.name}`}">{{ mon.name }}</RouterLink></li>
+
+  </ul>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
+import Home from '@/pages/Home.vue'
+import SpeciesInfo from '@/pages/SpeciesInfo.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{path: '/', component: Home}, {path: "/pokemon/:name", component: SpeciesInfo}],
 })
 
 export default router
