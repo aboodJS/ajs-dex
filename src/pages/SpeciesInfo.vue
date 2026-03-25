fce3b762c972ca2ee292cdc7e7e002bac5d89f3b
ajs <140010746+aboodJS@users.noreply.github.com>
2026-03-25 17:37:20 +0300

    the details page now shows the base stats of the pokemon

--- src/pages/SpeciesInfo.vue
+++ src/pages/SpeciesInfo.vue
@@ -11,11 +11,17 @@ const routeParams = useRoute()
 const url = `https://pokeapi.co/api/v2/pokemon/${routeParams.params.name}`
 const data = ref()
 const sprite = ref()
+const typeOne = ref()
+const typeTwo = ref()
+const baseStats = ref()
 
 async function getData() {
   await fetch(url).then(data => data.json()).then(result => data.value = result)
-  console.log(data.value)
   sprite.value = data.value.sprites.other["official-artwork"].front_default
+  typeOne.value = data.value.types[0].type.name
+  typeTwo.value = data.value.types.length > 1 ? data.value.types[1].type.name : ""
+  baseStats.value = data.value.stats
+  console.log(baseStats.value)
 }
 
 onMounted(() => {
@@ -28,15 +34,16 @@ onMounted(() => {
   <main>
     <h1>{{ $route.params.name }}</h1>
     <img class="h-54" :src="sprite" alt="" srcset="">
-    <!-- <div>
-    <span>{{ data.types[0].type.name }}</span>
-    <span>{{ data.types[1].type.name !== undefined ? data.types[1].type.name : "" }}</span>
+    <div>
+    <span>{{ typeOne }}</span>
+    <span>{{ typeTwo }}</span>
   </div>
+
 
     <ul>
       <h1>base stats</h1>
-      <li v-for="stat in data.stats">{{ stat.stat.name }}: {{ stat.base_stat }}</li>
-    </ul> -->
+      <li v-for="stat in baseStats">{{ stat.stat.name }}: {{ stat.base_stat }}</li>
+    </ul>
 
 
   </main>
