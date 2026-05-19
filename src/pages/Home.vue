96d614aca2199c4292ac65611a228e668150e1d0
ajs <140010746+aboodJS@users.noreply.github.com>
2026-05-19 23:24:52 +0300

    created the branch for the data fetching system rewrite

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,17 +10,10 @@ const pokemon: Ref<[Object]> = ref([])
 const query = ref("")
 
 
-const filteredSearch = computed(() => {
-  return pokemon.value.filter((p) => p.name.includes(query.value))
-})
 
-async function getSpecies() {
-  await dex.getPokemonSpeciesList().then(d => pokemon.value = d.results)
-  console.log(pokemon.value[0].url)
-}
+
 
 onMounted(() => {
-  getSpecies()
 })
 </script>
 
